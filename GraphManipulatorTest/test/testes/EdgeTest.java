02c616bf0451ed501e86dc4ba7e5a4174d71c622
Joao Maurício <joao.carvalho@ccc.ufcg.edu.br>
2018-06-04 20:34:28 -0300

    Adicionando testes da classe Edge

--- GraphManipulatorTest/test/testes/EdgeTest.java
+++ GraphManipulatorTest/test/testes/EdgeTest.java
@@ -2,13 +2,55 @@ package testes;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
+import manipulator.Edge;
+import manipulator.Vertex;
+
 import org.junit.Test;
+
+import junit.framework.Assert;
 
 public class EdgeTest {
 
+	Vertex end, start;
+	Edge edge;
+	
+	@Before
+	public void setup() {
+		end = new Vertex(2);
+		start = new Vertex(1);
+		edge = new Edge(end, start, 10f);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetEnd() {
+		Assert.assertEquals(end, edge.getEnd());
+	}
+	
+	@Test
+	public void testGetStart() {
+		Assert.assertEquals(start, edge.getStart());
+	}
+	
+	public void testSetEnd() {
+		Vertex newvertex = new Vertex(3);
+		edge.setEnd(newvertex);
+		Assert.assertEquals(newvertex, edge.getEnd());
+	}
+	
+	public void testSetStart() {
+		Vertex newvertex = new Vertex(3);
+		edge.setStart(newvertex);
+		Assert.assertEquals(newvertex, edge.getStart());
+	}
+	
+	public void testGetWeight() {
+		Assert.assertEquals(10f, edge.getWeight());
 	}
 
+	public void testSetWeight() {
+		edge.setWeight(9f);
+		Assert.assertEquals(9f, edge.getWeight());
+	}
 }
