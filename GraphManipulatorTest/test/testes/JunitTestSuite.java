052f988eb4d089fee43a4ac0163e0e00a865d2a2
João Pedro <joao.costa@ccc.ufcg.edu.br>
2018-06-04 20:11:46 -0300

    Enviando umas coisas

---
+++ GraphManipulatorTest/test/testes/JunitTestSuite.java
@@ -0,0 +1,11 @@
+package testes;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ EdgeTest.class, GraphManipulatorTest.class, GraphTest.class, VertexTest.class })
+public class JunitTestSuite {
+
+}
